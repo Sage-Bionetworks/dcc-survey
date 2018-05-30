263dceb97727bf839fd63bb72212b58348e8c9a5
Kara Woo <woo.kara@gmail.com>
2018-05-30 11:36:37 -0700

    Write submitted data to JSON file within data/ directory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import json
+import datetime
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +10,18 @@ def show_survey():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-  print(json.dumps(request.form))
+  directory = 'data'
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+  filename = datetime.datetime.now().isoformat()
+  filename_suffix = ".json"
+
+  path = os.path.join(directory, filename + filename_suffix)
+
+  with open(path, 'w') as outfile:
+    json.dump(request.form, outfile)
+
   return("hello")
 
 if __name__ == '__main__':
