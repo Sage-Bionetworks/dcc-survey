b94ce4bc87f1bc3ccdc4b2c60f12e2e0e14d6e97
Kara Woo <woo.kara@gmail.com>
2018-06-21 11:46:44 -0700

    Update survey instrument

--- static/index.js
+++ static/index.js
@@ -11,12 +11,18 @@ var surveyJSON = {
     {
      "type": "html",
      "name": "explanation",
-     "html": "<h4>Background</h4>\n\n<style>\np    {font-size: 14px;}\nli    {font-size: 14px;}\n</style>\n\n<p>\n  Funding for this grant is provided with the expectation that awardees engage in broad sharing of data, analytical methodology, and other resources in compliance with these data sharing terms, and in accordance with general NIH data sharing policies (NOT-OD-14-124: <a href=\"https://grants.nih.gov/grants/guide/notice-files/NOT-OD-14-124.html\">https://grants.nih.gov/grants/guide/notice-files/NOT-OD-14-124.html</a>)\n</p>\n\n<p>\n  To this end:\n</p>\n\n<ul>\n<li>all datasets used in support of and generated through this project will be made accessible and reusable by qualified individuals other than the original data generators.\n<li>all analytical methodologies will be made fully reproducible and transparent  so that results can be vetted and existing analysis techniques quickly applied to new application areas\n<li>all models of biological systems and networks will be made open to users such that  theoretical predictions can be rapidly validated experimentally.\n<li>all disease models generated in the course of the project will be made freely available to qualified investigators to accelerate their characterization and validation and their translational utility.\n</ul>\n\n<p>\nThe sharing of all content and data integration activities are provided by Sage Bionetworks through the AMP-AD Knowledge Portal (ampadportal.org), a collaborative data platform which serves as an IRB approved environment where data can be stored/accessed and collaboratively analyzed. Data will be made available broadly to all qualified users twice a year. There will be no publication embargo imposed on the use of data after they have been made available through the public portal.\n</p>"
+     "html": "<h4>Background</h4>\n\n<style>\np    {font-size: 14px;}\nli    {font-size: 14px;}\n</style>\n\n<p>\n  Funding for this grant is provided with the expectation that awardees engage in broad sharing of data, analytical methodology, and other resources in compliance with these data sharing terms, and in accordance with general NIH data sharing policies (NOT-OD-14-124: <a href=\"https://grants.nih.gov/grants/guide/notice-files/NOT-OD-14-124.html\">https://grants.nih.gov/grants/guide/notice-files/NOT-OD-14-124.html</a>)\n</p>\n\n<p>\n  To this end:\n</p>\n\n<ul>\n<li>all datasets used in support of and generated through this project will be made accessible and reusable by qualified individuals other than the original data generators.\n<li>all analytical methodologies will be made fully reproducible and transparent  so that results can be vetted and existing analysis techniques quickly applied to new application areas\n<li>all models of biological systems and networks will be made open to users such that  theoretical predictions can be rapidly validated experimentally.\n<li>all disease models generated in the course of the project will be made freely available to qualified investigators to accelerate their characterization and validation and their translational utility.\n</ul>\n\n<p>\nThe sharing of all content and data integration activities are provided by Sage Bionetworks through the AMP-AD Knowledge Portal (ampadportal.org), a collaborative data platform which serves as an IRB approved environment where data can be stored/accessed and collaboratively analyzed. Data will be made available broadly to all qualified users twice a year. There will be no publication embargo imposed on the use of data after they have been made available through the public portal.\n</p>\n\n<h4>Survey</h4>\n\n<p>\nPlease complete the following survey about your planned study and anticipated yearly milestones. Note that surveys in progress cannot be saved.\n</p>\n"
     },
     {
      "type": "text",
      "name": "foa_number",
      "title": "FOA number",
+     "isRequired": true
+    },
+    {
+     "type": "text",
+     "name": "grant_number",
+     "title": "Grant number",
      "isRequired": true
     },
     {
@@ -40,8 +46,8 @@ var surveyJSON = {
     },
     {
      "type": "radiogroup",
-     "name": "new_data",
-     "title": "Will project generate new data?",
+     "name": "reuse_data",
+     "title": "Will project reuse existing data?",
      "isRequired": true,
      "choices": [
       "yes",
@@ -51,36 +57,20 @@ var surveyJSON = {
     {
      "type": "comment",
      "name": "data_source",
-     "visibleIf": "{new_data} = \"no\"",
+     "visibleIf": "{reuse_data} = \"yes\"",
      "title": "Describe existing data source. If data is publicly available, please provide a link."
-    },
-    {
-     "type": "radiogroup",
-     "name": "preliminary_data",
-     "title": "Has preliminary data been generated in support of the study?",
-     "isRequired": true,
-     "choices": [
-      "yes",
-      "no"
-     ]
-    },
-    {
-     "type": "comment",
-     "name": "preliminary_data_desc",
-     "visibleIf": "{preliminary_data} = \"yes\"",
-     "title": "Describe the preliminary data"
     },
     {
      "type": "comment",
      "name": "study_summary",
      "title": "Study summary",
-     "description": "Please describe the planned study(s) in as much detail as possible, including a summary of experimental design(s), human data or model systems, assays that will be used, and estimated total number of samples or measures from which data will be generated for each assay over the lifespan of the grant.",
+     "description": "Please provide a succinct summary of the planned study, including its experimental design, use of human data or model systems, and inclusion/exclusion criteria.",
      "isRequired": true
     },
     {
      "type": "paneldynamic",
      "name": "timeline",
-     "title": "Timeline",
+     "title": "Timeline of data contributions",
      "isRequired": true,
      "templateElements": [
       {
@@ -118,21 +108,21 @@ var surveyJSON = {
        "name": "experimental_data_desc",
        "visibleIf": "{panel.expected_contribs} contains \"experimentalData\"",
        "title": "Experimental data",
-       "description": "Describe what type of data will be contributed. Include source of biosamples (brain region, cell types etc) and what assays will be generated from them, or tests or assessments of living individuals (such as cognitive tests, behavioral analysis or imaging). Include the number of individuals, animals, or batches of cultured cells data will be contributed from."
+       "description": "Describe what type of data will be shared. Include source of biosamples (brain region, cell types etc) and what assays will be generated from them, or tests or assessments of living individuals (such as cognitive tests, behavioral analysis or imaging). Include the number of individuals, animals, or batches of cultured cells data will be contributed from."
       },
       {
        "type": "comment",
        "name": "analysis_desc",
        "visibleIf": "{panel.expected_contribs} contains \"analysis\"",
        "title": "Analysis",
-       "description": "Describe the type of analysis that will be contributed and the source of the data that was analyzed."
+       "description": "Describe the type of analysis that will be shared and the source of the data that was analyzed. Also indicate if any analytical code will be shared."
       },
       {
        "type": "comment",
        "name": "tool_desc",
        "visibleIf": "{panel.expected_contribs} contains \"tool\"\n",
        "title": "Tool",
-       "description": "Describe tools that will be made available, such as vectors and animal models."
+       "description": "Describe any research or analytical tools that will be made available, such as vectors, animal models, and reusable software."
       },
       {
        "type": "comment",
